6f94df8ed3ec42fc0df522d2439d5154fbcb583e
Michael Bailey <mbailey887@gmail.com>
2020-04-29 12:53:00 -0500

    created individual pizza name pages

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -9,4 +9,8 @@ app_name = 'pizzas'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all pizza names
+    path('pizzanames/', views.pizzanames, name='pizzanames'),
+    # Detail page for a single pizza
+    path('pizzanames/<int:pizzaname_id>/', views.pizzaname, name='pizzaname'),
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import PizzaName
+
 def index(request):
     """The home page for Pizzeria."""
     return render(request, 'pizzas/index.html')
+
+def pizzanames(request):
+    """Show all pizza names."""
+    pizzanames = PizzaName.objects.order_by('text')
+    context = {'pizzanames': pizzanames}
+    return render(request, 'pizzas/pizzanames.html', context)
+
+def pizzaname(request, pizzaname_id):
+    """Show a single pizza and all its toppings."""
+    pizzaname = PizzaName.objects.get(id=pizzaname_id) #pizzaname_id might not be right
+    toppings = pizzaname.topping_set.order_by('pizzaname_id')
+    context = {'pizzaname': pizzaname, 'toppings': toppings}
+    return render(request, 'pizzas/pizzaname.html', context) 
